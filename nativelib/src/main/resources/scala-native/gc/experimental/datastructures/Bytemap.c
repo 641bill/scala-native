6a41898a7e3f6816c5e74d5c208e1a8bb21c6e0f
641bill <wo23636@126.com>
2023-07-10 19:58:58 +0800

    Fix the code that got forgotten adding conditional compilation

--- nativelib/src/main/resources/scala-native/gc/experimental/datastructures/Bytemap.c
+++ nativelib/src/main/resources/scala-native/gc/experimental/datastructures/Bytemap.c
@@ -1,3 +1,5 @@
+#if defined(SCALANATIVE_GC_EXPERIMENTAL)
+
 #include "Bytemap.h"
 #include <stdio.h>
 #include "immix_commix/utils/MathUtils.h"
@@ -9,3 +11,5 @@ void Bytemap_Init(Bytemap *bytemap, word_t *firstAddress, size_t size) {
     assert(Bytemap_index(bytemap, (word_t *)((ubyte_t *)(firstAddress) + size) -
                                       ALLOCATION_ALIGNMENT) < bytemap->size);
 }
+
+#endif
